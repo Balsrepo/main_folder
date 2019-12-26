8aecee8f824a3871883255e56fce25b813bfd5fc
Balsrepo <balabals24@icloud.com>
2019-12-26 21:09:55 +0100

    few updates

--- python3_challenges/README.md
+++ python3_challenges/README.md
@@ -1,4 +1,13 @@
 # Python Challenges from edabit.com/challenges/python3
-##All in one file and will be updated periodically !
 
-Folder structure:
+All levels are in seperate files and it will be updated periodically !
+
+---------------
+List of Levels:
+---------------
+Very easy - Level 0
+Easy - Level 1
+Medium - Level 2
+ - Level 
+ - Level 
+ - Level 
--- python3_challenges/very_easy.py
+++ python3_challenges/very_easy.py
@@ -1,6 +1,27 @@
 # All titles of the challenges are commented on top of the functions
+# Functions can be called and return statements can be replace with print statement 
+
 
 #Return the sum of two numbers
 def sum_of_2_nums(a,b):
-    return a+b
-    #print(a+b)
+    return a+b 
+
+#Return the Next Number from the Integer Passed
+def return_next_number(num):
+    return num+1
+
+#Convert Minutes into Seconds
+def mins_to_secs(mins):
+    return mins*60
+
+#Area of Triangle #where b: base h: height
+def area_of_traingle(b,h):
+    return (b*h)/2
+
+#Convert Hours into Seconds
+def hours_into_secs(hours):
+    return 3600*hours # i.e. 1 hour is equal to 3600 seconds
+
+#Maximum Edge of a Triangle
+def max_edge_of_traingle(a,b):
+    pass
