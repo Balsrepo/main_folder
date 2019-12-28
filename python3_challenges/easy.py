3420d0c4de67545ca9ba98b655c93a0fbbbeb6f2
Balsrepo <balabals24@icloud.com>
2019-12-28 13:28:36 +0100

    new updates

--- python3_challenges/easy.py
+++ python3_challenges/easy.py
@@ -52,14 +52,60 @@ def equality_of_3_values(a,b,c):
 def isogram(input_string):
     pass
 
-#Return the List of Sublists
-def list_of_sublists(list_of_lists):
-    pass
-
-#Return the Index of All Capital Letters
+# Return the List of Sublists
+# Write a function that takes three arguments (x, y, z) and returns a list containing x sublists (e.g. [[], [], []]), each containing y number of item z.
+# x Number of sublists contained within the main list.
+# y Number of items contained within each sublist.
+# z Item contained within each sublist.
+def list_of_sublists(x,y,z):
+    import numpy as np
+    arr = np.zeros([x,y])
+    arr = arr.tolist()
+    for i in range(x):
+        for j in range(y):
+            arr[i][j] = z
+    return arr
 
 #Remove Every Vowel from a String
+def remove_vowel_from_string(input_string):
+    vowels = """aeiouAEIOU""" # works also with capitalized letters
+    removed_vowels = ""
+    for i in input_string:
+        if i not in vowels:
+            removed_vowels = removed_vowels + i
+    return removed_vowels
+
+#Return the Index of All Capital Letters
+def index_of_all_capital_letters(input_letters):
+    indexes=[]
+    for i in range(len(input_letters)):
+        if input_letters[i].isupper():
+            indexes.append(i)
+    return indexes
 
 #Snail Race
 
+
 #Count Letters in a Word Search
+
+
+#ATM PIN Code Validation
+def isValidPIN(input_pin):
+    length = len(input_pin)
+    if length==4 or length==6 and input_pin.isdigit():
+        return True
+    else:
+        return False
+
+#Total Volume
+# Given a list of boxes, create a function that returns the total volume of all those boxes combined together. 
+# A box is represented by a list with three elements: length, width and height.
+# For instance, total_volume([2, 3, 2], [6, 6, 7], [1, 2, 1]) 
+# should return 266 since (2 x 3 x 2) + (6 x 6 x 7) + (1 x 2 x 1) = 12 + 252 + 2 = 266.
+def total_volume(lists):
+    test=1
+    for i in lists:
+        test = test * i
+    return volume
+
+print(total_volume([1,2,3]))
